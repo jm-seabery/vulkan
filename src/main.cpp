6f3862abf1ec07f24c0c768957da62664780a7d2
John Murray <jm@seaberyat.com>
2023-05-25 14:19:31 +0200

    Basic app mounted with submodules

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+
+#include "glm/glm.hpp"
+#include "vulkan/vulkan.hpp"
+
+int main()
+{
+    std::cout << "App Ready" << std::endl;
+    std::string testString { "ONETWOThree" };
+
+    return 0;
+}
